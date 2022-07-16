fb9236f22ff3b952478ac1f91d8a492cbf06eab9
hamidahmaulida <hamidahmaulida@students.amikom.ac.id>
2022-07-16 10:21:10 +0700

    inisialisasi project

---
+++ Mahasiswa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+            public string Nim { get; set; }
+            public string Nama { get; set; }
+            public string JenisKelamin { get; set; }
+            public string IPK { get; set; }
+        }
+}
